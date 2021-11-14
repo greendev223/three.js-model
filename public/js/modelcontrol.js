1413bf58f3d82489a4ef5cabd29d763182cd86ec
Zeus <84690417+darkrut22@users.noreply.github.com>
2021-11-15 01:44:43 +0900

    add three.js GUI

--- public/js/modelcontrol.js
+++ public/js/modelcontrol.js
@@ -8,19 +8,57 @@ import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/js
 import Stats from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/libs/stats.module.js';
 import { GUI } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/libs/dat.gui.module.js';
 var scene, camera, renderer
+var car_model;
+var global_gui;
+var camX=0, camY=-50, camZ=50;
+var carX=-30, carY=-40, carZ=0, carRotate = 0, carScale = 0.3;
+var options = {
+  carX: -30,
+  carY: -40,
+  carRotate: 0,
+  camX: 0,
+  camY: -50,
+  camZ: 50,
+  camera_control : false,
+  camera: {
+    speed: 0.0001
+  },
+  reset: function() {
+    this.carX = -30;
+    this.carY = -40;
+    this.carRotate = 0;
+    this.camX = 0;
+    this.camY = -50;
+    this.camZ = 50;        
+    this.camera_control = false;
+    car_model.position.set(carX, carY, carZ);
+    camera.position.set(camX, camY, camZ);
+  }
+};
+
+function create_scene() {  
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color('skyblue'); 
+}
 
 function create_camera() {
-  let cam = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 1, 1000 );  
-  cam.position.set(0,-50,50);
-  cam.lookAt(new THREE.Vector3(0, 0, 0));
+  camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 1, 1000 );  
+  camera.position.set(camX, camY, camZ);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
   //add gui of camera
-  let gui = new GUI();
-  let cameraFolder = gui.addFolder('Camera')
-  cameraFolder.add(cam.position, 'x', -100, 100)
-  cameraFolder.add(cam.position, 'y', -100, 100)
-  cameraFolder.add(cam.position, 'z', -100, 100)
+  let cameraFolder = global_gui.addFolder('Camera')
+  cameraFolder.add(camera.position, 'x', -100, 100).listen();
+  cameraFolder.add(camera.position, 'y', -100, 100).listen();
+  cameraFolder.add(camera.position, 'z', -100, 100).listen();
   cameraFolder.open()
-  return cam;
+}
+
+function create_renderer() {
+  // create rendering
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  renderer.shadowMap.enabled = true;
 }
 
 function create_axes(params) {
@@ -45,43 +83,36 @@ function create_ground(w, h) {
 
 function create_light() {
   const light = new THREE.PointLight( 0xffffff, 1, 1000, 1 );
-  light.position.set( -100, 100, 200 );
+  light.position.set( -200, 200, 300 );
   light.castShadow = true; // default false
   scene.add( light );
 
   const light1 = new THREE.PointLight( 0xffffff, 1, 1000, 1 );
-  light1.position.set( 100, -100, 200 );
+  light1.position.set( 200, -200, 300 );
   light1.castShadow = true; // default false
   scene.add( light1 );
 }
 
 function import_model(name,url) {  
   let loader = new GLTFLoader();
-  loader.load(url, function(gltf){
-    var _obj=gltf.scene.children[0];        
-    _obj.scale.set(0.2,0.2,0.2);
-    scene.add(gltf.scene);
-    _obj.rotateX(Math.PI/2);
-    _obj.position.set(-30, -40, 0);    
-    let gui = new GUI()
+  loader.load(url, function(model_gltf){
+    car_model=model_gltf.scene.children[0];      
+    car_model.scale.set(carScale,carScale,carScale);
+    scene.add(model_gltf.scene);
+    car_model.rotateX(Math.PI/2);
+    car_model.position.set(carX, carY, carZ); 
     //add gui of car
-    let _objFolder = gui.addFolder(name)
-    _objFolder.add(_obj.scale, 'x', 0.1, 0.3)
-    _objFolder.add(_obj.scale, 'y', 0.1, 0.3)
-    _objFolder.add(_obj.scale, 'z', 0.1, 0.3)
-    _objFolder.open()
+    let car_modelFolder = global_gui.addFolder(name)
+    car_modelFolder.add(car_model.position, 'x', -100, 100).listen();
+    car_modelFolder.add(car_model.position, 'y', -100, 100).listen();
+    car_modelFolder.add(options,'carRotate', -3.141592, 3.141592).name('rotate').listen();
+    car_modelFolder.open()
+    
+    global_gui.add(options, 'camera_control').listen();
+    global_gui.add(options,"reset");
   }, undefined, function ( error ) {
     console.error( error );
   });
-}
-
-function create_renderer() {
-  // create rendering
-  renderer = new THREE.WebGLRenderer();
-  renderer.shadowMap.enabled = true;
-  document.body.appendChild(renderer.domElement);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
 }
 
 function onWindowResize() {
@@ -90,34 +121,48 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-function init() {
-  //************************ Creat Scene ****************************** */
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color('skyblue'); 
-  //Add stats
-  const stats = Stats()
-  document.body.appendChild(stats.dom)
+// function create_gui() {
+//   let gui = new GUI;
+  
+//   global_gui = gui.addFolder('Controls');
+//   global_gui.add(options, 'Camera');
+//   global_gui.add(options, 'Car');
+//   global_gui.open();
+// }
 
-  camera = create_camera();
+function init() {  
+  global_gui = new GUI();
+  create_scene();
+  create_camera();
+  create_renderer();
   create_axes();
   create_ground(2000, 2000);
   create_light();
   import_model('Car', '/models/cars/fenyr_super_sport/scene.gltf');//importing model of car
-  create_renderer();
+  //Add stats
+  const stats = Stats()
+  document.body.appendChild(stats.dom)
   // add control of camera
   var cameral_control = new OrbitControls(camera, renderer.domElement);
   cameral_control.addEventListener('change', renderer);
 
   window.addEventListener('resize', onWindowResize)
-
-  update(renderer, scene, camera);
+  
+  scene_rendering();
 }
+// function animate() {
+//   requestAnimationFrame(animate)
+//   cube.rotation.x += 0.01
+//   cube.rotation.y += 0.01
+//   controls.update()
+//   render()
+//   stats.update()
+// }
 
-function update(renderer, scene, camera) {
-  renderer.render(scene, camera);            
-  requestAnimationFrame(function() {
-    update(renderer, scene, camera);
-  })
+function scene_rendering() {
+	requestAnimationFrame( scene_rendering );
+	renderer.render( scene, camera );
+  
 }
 
 init();
