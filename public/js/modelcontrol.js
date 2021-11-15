b3ec87fb6f7d7888a0d70fa2776585226568a7c1
Zeus <84690417+darkrut22@users.noreply.github.com>
2021-11-15 17:19:17 +0900

    add animation

--- public/js/modelcontrol.js
+++ public/js/modelcontrol.js
@@ -13,35 +13,49 @@ var stats
 var car_model;
 var global_gui;
 var car_set = {};
-var camera_set = {};
-car_set.px = 0;car_set.py = 0;car_set.pz = 0;car_set.scale = 0.3;
-camera_set.cx = car_set.px;camera_set.cy = car_set.py;camera_set.cz = car_set.pz;
-camera_set.px = car_set.px;camera_set.py = car_set.py+300;camera_set.pz = car_set.pz + 300;
-var carlight, spherelight, carlight_target, light;
-
+car_set.px = -13;car_set.py = 0;car_set.pz = 0;
+var pointlight1, pointlight2, light;
+var animation_flag = false;
+var time = 0;
+var newPosition = new THREE.Vector3();
+var matrix = new THREE.Matrix4();
 
 var options = {
   carX: 0,
-  carY: 0,
-  carRotate: 0,
+  carY: 0,  
   camX: 0,
   camY: 0,
   camZ: 0,
-  camera_control : false,
-  camera: {
-    speed: 0.0001
+  animation_view:function () {
+    if (!animation_flag){
+      animation_flag = true;
+      // camera.rotation.x=Math.PI;
+      light.intensity = 1.2;
+      light.distance = 500;
+      pointlight1.visible = true;
+      pointlight2.visible = true;
+    } else{
+      animation_flag = false;
+      light.intensity = 1.2;
+      light.distance = 2000;
+      pointlight1.visible = false;
+      pointlight2.visible = false;      
+    }
   },
   reset: function() {
+    car_model.position.set(car_set.px, car_set.py, car_set.pz);
+    light.distance = 2000;
+    light.intensity = 1.2;
+    pointlight1.visible = false;
+    pointlight2.visible = false;
+    animation_flag = false;
     this.carX = car_set.px;
     this.carY = car_set.py;
     this.carRotate = 0;
-    this.camX = camera_set.px;
-    this.camY = camera_set.py;
-    this.camZ = camera_set.pz;        
-    this.camera_control = false;
-    car_model.position.set(car_set.px,car_set.py, car_set.pz);
-    camera.position.set(camera_set.px, camera_set.py, camera_set.pz);    
-    camera.lookAt(new THREE.Vector3(camera_set.cx, camera_set.cy, camera_set.cz));
+    this.camX = car_set.px;
+    this.camY = car_set.py-300;
+    this.camZ = car_set.pz+300;  
+    mapping_value();
   }
 };
 
@@ -51,14 +65,18 @@ function create_scene() {
 }
 
 function create_camera() {
-  camera = new THREE.PerspectiveCamera( 300, window.innerWidth/window.innerHeight, 1, 1000 );  
-  camera.position.set(camera_set.px, camera_set.py, camera_set.pz);  
-  camera.lookAt(new THREE.Vector3(camera_set.cx, camera_set.cy, camera_set.cz));
-  //add gui of camera
+  // camera = new THREE.PerspectiveCamera( 10000, window.innerWidth/window.innerHeight, 1, 1000 );  
+  camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.set(0, car_set.py-300, car_set.pz+300);  
+  // camera.rotateX(-Math.PI);
+  // camera.rotateY(-Math.PI);
+  // camera.lookAt(new THREE.Vector3(0, car_set.py, car_set.pz));
+  // camera.rotateY(-Math.PI);
+  camera.lookAt( scene.position );  
   let cameraFolder = global_gui.addFolder('Camera')
   cameraFolder.add(camera.position, 'x', -1000, 1000).listen();
   cameraFolder.add(camera.position, 'y', -1000, 1000).listen();
-  cameraFolder.add(camera.position, 'z', -200, 200).listen();
+  cameraFolder.add(camera.position, 'z', -1000, 1000).listen();
   cameraFolder.open()
 }
 
@@ -91,39 +109,59 @@ function create_ground(w, h) {
 }
 
 function create_light() {
-  light = new THREE.PointLight( 0xffffff, 1, 200, 1 );
-  // light.position.set( -200, 200, 200 );
+  light = new THREE.PointLight( 0xffffff, 1.2, 2000, 1 );  
   light.castShadow = true; // default false
-  scene.add( light );
-  
-  const geometry = new THREE.SphereGeometry( 1, 32, 16 );
-  const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-  spherelight = new THREE.Mesh( geometry, material );
-  scene.add( spherelight );
-  carlight = new THREE.SpotLight( 0xffffff, 2, 150, 0.40 );
-  scene.add(carlight);
-  carlight_target = new THREE.Object3D();
-  scene.add(carlight_target);  
-  carlight.target = carlight_target;
+  light.position.set(car_set.px+37, car_set.py+70, car_set.pz+300);
 
+  const geometry = new THREE.SphereGeometry( 0.3, 8, 8 );
+  const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  pointlight1 = new THREE.Mesh( geometry, material );
+  pointlight2 = new THREE.Mesh( geometry, material );
+
+  var carlight1 = new THREE.SpotLight( 0xffffff, 2, 500, 0.3 );  
+  var carlight2 = new THREE.SpotLight( 0xffffff, 2, 500, 0.3 );  
+
+  var car_target1 = new THREE.Object3D();
+  var car_target2=  new THREE.Object3D();  
+  
+  pointlight1.position.set(car_set.px+26, car_set.py+6, car_set.pz+7)
+  carlight1.position.set(0,0,0);  
+  car_target1.position.set(0,-20,0);
+  carlight1.target = car_target1;
+  pointlight1.add(carlight1);
+  pointlight1.add(car_target1);  
+  
+  pointlight2.position.set(car_set.px+88, car_set.py+6, car_set.pz+7)
+  carlight2.position.set(0,0,0);  
+  car_target2.position.set(0,-20,0);    
+  carlight2.target = car_target2;
+  pointlight2.add(carlight2);
+  pointlight2.add(car_target2);  
+
+  // pointlight1.visible = false;
+  // pointlight2.visible = false;
 }
 
 function import_model(name,url) {  
   let loader = new GLTFLoader();
   loader.load(url, function(model_gltf){
     car_model=model_gltf.scene.children[0];      
-    car_model.scale.set(car_set.scale, car_set.scale, car_set.scale);
-    scene.add(model_gltf.scene);
+    car_model.scale.set(0.3, 0.3, 0.3);
     car_model.rotateX(Math.PI/2);
+    car_model.add(camera);
+    car_model.add(light);
+    car_model.add(pointlight1);
+    car_model.add(pointlight2);
+    scene.add(model_gltf.scene);
     car_model.position.set(car_set.px, car_set.py, car_set.pz); 
     //add gui of car
     let car_modelFolder = global_gui.addFolder(name)
     car_modelFolder.add(car_model.position, 'x', -1000, 1000).listen();
     car_modelFolder.add(car_model.position, 'y', -1000, 1000).listen();
-    car_modelFolder.add(options,'carRotate', -3.141592, 3.141592).name('rotate').listen();
+    car_modelFolder.add(car_model.position,'z', -1000, 1000).listen();
     car_modelFolder.open()
     
-    global_gui.add(options, 'camera_control').listen();
+    global_gui.add(options, 'animation_view').name('Animation On/OFF');
     global_gui.add(options,"reset");
   }, undefined, function ( error ) {
     console.error( error );
@@ -145,6 +183,7 @@ function init() {
   create_ground(2000, 2000);
   create_light();
   import_model('Car', '/models/cars/fenyr_super_sport/scene.gltf');//importing model of car
+  
   //Add stats
   stats = Stats();
   document.body.appendChild(stats.dom)
@@ -157,20 +196,25 @@ function init() {
   scene_rendering();
 }
 
-function scene_rendering() {
+function scene_rendering() {  
 	requestAnimationFrame( scene_rendering );
-  car_model.position.y -= 2;
-  if(car_model.position.y<-1000){
-    car_model.position.y=1000;
-  }  
-  camera.rotation.x=Math.PI;
-  camera.position.set(car_model.position.x, car_model.position.y+200, car_model.position.z+200);
-  camera.lookAt(new THREE.Vector3(car_model.position.x, car_model.position.y, car_model.position.z));
-  light.position.set(car_model.position.x+13, car_model.position.y+30, car_model.position.z+100);
-  carlight.position.set(car_model.position.x+13, car_model.position.y+3, car_model.position.z+5);
-  spherelight.position.set(car_model.position.x+13, car_model.position.y+3, car_model.position.z+5);
-  carlight_target.position.set(car_model.position.x+13, car_model.position.y-100, car_model.position.z+5);
-  // camera.rotation.z +=0.01;
+  if(animation_flag) {        
+    newPosition.x = car_model.position.x;
+    newPosition.y = car_model.position.y-0.5;
+    newPosition.z = car_model.position.z;
+    car_model.position.copy(newPosition);
+
+    var newLookAt = new THREE.Vector3();
+    newLookAt.x=car_model.position.x;
+    newLookAt.y=car_model.position.y-200;
+    newLookAt.z=car_model.position.z-200;
+    // car_model.lookAt(newPosition);
+
+    if(car_model.position.y<-1000){
+      car_model.position.y=1000;
+    }
+    
+  }
 	renderer.render( scene, camera );
   stats.update()
 }
